a1164f73436f66c8c96be023853c04e9782ca227
Daria_Ketova <dariaket@mail.ru>
2023-05-18 15:04:29 +0300

    раскидала по папкам

--- scripts.js
+++ scripts.js
@@ -1,7 +1,63 @@
 const cards = document.querySelectorAll('.game-card');
 
+let CardIsFlipped = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-  this.classList.toggle('flip');
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!CardIsFlipped) {
+    CardIsFlipped = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+
+  checkForMatch();
 }
+
+function checkForMatch() {
+  if (firstCard.dataset.number === secondCard.dataset.number){
+  return disableCards();
+ } else{
+  return unflipCards();
+ }
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
